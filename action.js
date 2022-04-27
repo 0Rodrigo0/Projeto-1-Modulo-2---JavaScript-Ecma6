fc893350b9da46aa8ea3c71f57951ba6500d8f04
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2022-04-26 21:02:48 -0300

    validação input

--- action.js
+++ action.js
@@ -5,17 +5,8 @@ const lisItens = document.querySelector(".list-itens");
 
 // evento dos botões
 document.addEventListener("DOMContentLoaded", getValuesDom);
-//addBtn.addEventListener("click", addItemToList);
+addBtn.addEventListener("click", addItemToList);
 lisItens.addEventListener("click", deleteItens);
-lisItens.addEventListener("click", popUp);
-addBtn.addEventListener("click", function (event) {
-  if (document.querySelector(".input").value == "") {
-    return false;
-  } else {
-    addEventListener("click", addItemToList);
-    location.reload();
-  }
-});
 
 // funções
 function addItemToList(event) {
@@ -29,6 +20,7 @@ function addItemToList(event) {
   // criando checkbox
   const checkBox = document.createElement("input");
   checkBox.type = "checkbox";
+  checkBox.id = "chkbx";
   checkBox.classList.add("check-box");
   divItens.appendChild(checkBox);
 
@@ -38,58 +30,60 @@ function addItemToList(event) {
   novaliItens.classList.add("li-class");
   divItens.appendChild(novaliItens);
 
-  // salva no localStorage
-  saveLocalStorage(inputText.value);
-
   //criando botão de lixo e a classe
   const trash = document.createElement("button");
   trash.innerHTML = '<i class="icon-x"></i>';
   trash.classList.add("trash-btn");
   divItens.appendChild(trash);
 
-  //adiciona todos itens acima na lista
-  lisItens.appendChild(divItens);
+  if (inputText.value === "") {
+    alert("Nome do produto é necessário!");
+    return;
+  } else {
+    //adiciona todos itens acima na lista
+    lisItens.appendChild(divItens);
+    // salva no localStorage
+    saveLocalStorage(inputText.value);
+  }
+
   // limpa o imput
   inputText.value = "";
 }
 
+//valida se a entrada do popup é numero
 function isNumber(price) {
   return !isNaN(price);
 }
 
+// popup do valor dos produtos
 function popUp(event) {
   const item = event.target;
   if (item.classList[0] === "check-box") {
-    let price = prompt(
-      "Digite o valor: " + "\n" + "(use . para os centavos): "
-    );
-    if (price == null || price == "") {
-      let alert = alert("Você deve digitar um valor!");
+    let price = prompt("Digite o valor: " + "\n" + "(Ex: 10.45): ");
+    if (price == null || price == "" || isNumber(price) === false) {
+      alert = alert("Você deve digitar um valor numérico!");
     } else {
-      if (isNumber(price) === true) {
-        console.log(price);
-      } else {
-        alert = alert("Você deve digitar um valor!");
-        location.reload();
-      }
+      console.log(price);
+      savePriceOnLocalStorage(price);
     }
   }
 }
 
-function deleteItens(event) {
-  const item = event.target;
-  if (item.classList[0] === "trash-btn") {
-    const value = item.parentElement;
-    deleteLocalStorage(value);
-    value.remove();
+// salvando preço no localStorage
+function savePriceOnLocalStorage(value) {
+  let price;
+  // verificando localStorage ta vaziu, se esta cria o array, se nao traz os itens
+  if (localStorage.getItem("price") === null) {
+    price = [];
+  } else {
+    price = JSON.parse(localStorage.getItem("price"));
   }
-  /*if (item.classList[0] === "botao-lido") {
-    const tarefa = item.parentElement;
-    tarefa.classList.toggle("completed");
-  }*/
+  //add item no array e salva no localStorage
+  price.push(value);
+  localStorage.setItem("price", JSON.stringify(price));
 }
 
-// salvando no localStorage
+// salvando nomes no localStorage
 function saveLocalStorage(value) {
   let itens;
   // verificando localStorage ta vaziu, se esta cria o array, se nao traz os itens
@@ -103,6 +97,7 @@ function saveLocalStorage(value) {
   localStorage.setItem("itens", JSON.stringify(itens));
 }
 
+//traz os valores do localstorage
 function getValuesDom() {
   let itens;
   // verificando localStorage ta vaziu, se esta cria o array, se nao traz os itens
@@ -141,6 +136,21 @@ function getValuesDom() {
   });
 }
 
+// deleta itens da lista
+function deleteItens(event) {
+  const item = event.target;
+  if (item.classList[0] === "trash-btn") {
+    const value = item.parentElement;
+    deleteLocalStorage(value);
+    value.remove();
+  }
+  /*if (item.classList[0] === "botao-lido") {
+    const tarefa = item.parentElement;
+    tarefa.classList.toggle("completed");
+  }*/
+}
+
+//deleta itens do localstorage
 function deleteLocalStorage(value) {
   let itens;
   // verificando localStorage ta vaziu, se esta cria o array, se nao traz os itens
@@ -151,7 +161,6 @@ function deleteLocalStorage(value) {
   }
   // aqui ele esta buscando o valor contido no array do localStorage
   const valueIndex = value.children[0].innerText;
-  console.log(valueIndex);
 
   //remove do array
   itens.splice(itens.valueIndex, 1);
